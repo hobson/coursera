d035084a361262b34f686b43869322337f6503f7
Hobson Lane <hobsonlane@gmail.com>
2013-08-10 13:44:43 -0700

    correct errors

--- nlp/classifier.py
+++ nlp/classifier.py
@@ -12,7 +12,7 @@ def get_words(s):
     # only alpha words, no numbers
     splitter = re.compile(r'\W*')
     # filter after map, unlike collective intelligence which reverses this
-    return set(filter(get_words.filter_fun, map(get_words.map_fun, splitter.splitter(s))))
+    return filter(get_words.filter_fun, map(get_words.map_fun, splitter.split(s)))
 get_words.map_fun = str.lower
 get_words.max_len = 16
 get_words.min_len = 3
@@ -23,30 +23,31 @@ class Basic:
     def __init__(self, get_features=get_words, s='', path=''):
         self.get_features = get_features
         # {feature: total_num_occurrences}
-        self.feature_count = Counter()
+        self.feature_count = defaultdict(Counter)
         # {category: {feature: num_occurences,...}}
-        self.category_count = defaultdict(Counter)  # dict of Counters, count of documents binned in each category 
+        self.category_count = Counter()  # dict of Counters, count of documents binned in each category 
         self.num_items = 0
 
-    def increment_feature(self, feature, category):
+    def increment_feature_count(self, feature, category):
         "Increase the count for a feature<->category association"
-        self.feature_count[feature] += Counter((feature,))
+        # if isinstance(feature, (list, )):   # don't do tuple, or hasasstr(__iter__) in case feature = N-gram stored as tuple
+        #     self.feature_count[category] += Counter(feature)
         # self.feature_count.setdefault(feature, Counter())
         # self.feature_count[feature].setdefault(category, 0)
         # self.feature_count[feature][category] += 1
+        self.feature_count[category][feature] += 1
 
-    def increment_count(self, category):
+    def increment_category_count(self, category):
         "Increase the count of documents associated with a category"
-        self.category_count.setdefault(category, 0)
+        # self.category_count.setdefault(category, 0)
         self.category_count[category] += 1
 
     def count_feature_in_category(self, feature, category):
         "Count of feature occurences in a category"
         # using a defaultdict everywhere would take care of this
-        if feature in self.feature_count and category in self.feature_count[feature]:
-            return float(self.feature_count[feature][category])
-        return 0.
-
+        #if category in self.feature_count and feature in self.feature_count[category]:
+        return float(self.feature_count[category][feature])
+        
     def items_in_category(self, category):
         if category in self.category_count:
             return float(self.category_count[category])    
@@ -61,7 +62,20 @@ class Basic:
         return list(self.category_count)
 
     def train(self, string, category):
-        "Identify features (N-grams or words) in a string and associate that string with a category"
+        """Identify features (N-grams or words) in a string and associate that string with a category
+        >>> c = Basic()
+        >>> c.train('The quick brown fox jumps over the lazy dog', 'good')
+        >>> c.train('Make quick money in the online casino', 'bad')
+        >>> c.count_feature_in_category('quick', 'good')
+        1.0
+        >>> c.count_feature_in_category('quick', 'bad')
+        1.0
+        >>> c.num_items()
+        2
+        """
+        print self.get_features(string)
         self.feature_count[category] += Counter(self.get_features(string))
-
+        self.category_count[category] += 1
+        print self.category_count
+        print self.feature_count
 
