351ec60ac3b53721d10cda2a447c23ff6d9e5e5c
Hobson Lane <hobsonlane@gmail.com>
2013-12-23 11:13:26 -0800

    automata notes, count-words renamed count_words, basic word and sentence tokenizers

--- auto/notes and flash cards.md
+++ auto/notes and flash cards.md
@@ -1,9 +1,22 @@
+Notes
+-----
+
 Examples of Undecidable questions about programs or Turing machines, M, (since programs can simulate any Turing Machine) and their languages, L:
 
-Is L(M) a regular language?
-Is L(M) context-free?
-Does L(M) include any palindromes?
-I L(M) empty?
-Does L(M) contain more than N strings? (for any N)
+* Is L(M) a regular language?
+* Is L(M) context-free?
+* Does L(M) include any palindromes?
+* I L(M) empty?
+* Does L(M) contain more than N strings? (for any N)
 
+Undecidable problems that don't involve Turing machines
+
+* Is a particular CFG ambiguous?
+# Is a given CFL equal to all possible strings over its terminal alphabet?
+* Is a particular CFL a regular langage?
+
+Errata
+------
+
+Lecture 19, slide 21, refers to list of indexes i1,...ik and then uses the indexes calling them i1, ... in (w_i1...w_in = xi1...xin)
 
--- nlp/strutil.py
+++ nlp/strutil.py
@@ -1,3 +1,6 @@
+import re
+import nltk
+
 def strip_HTML(s):
     """Silly HTML stripper"""
     result = ''
@@ -12,6 +15,26 @@ def strip_HTML(s):
             result += c
     return result
 
+
+def get_words(s, regex='[^\'_-.a-zA-Z0-9]+|\W+\s+'):
+    if isinstance(regex, basestring):
+        regex = re.compile(regex)
+    return regex.split(s)
+
+
+def get_sentences(s, regex='[.?!](\W+)|'):
+    if isinstance(regex, basestring):
+        regex = re.compile(regex)
+    return regex.split(s)
+
+#!/usr/bin/env bash
+# segment, sort, and count words using nothing more than a bash oneliner
+
+# jurafsky's version that does word frequency analysis on an ascii file containing the works of Shakespear
+# tr 'A-Z' 'a-z' < shakes.txt | tr -sc 'A-Za-z' '\n' | sort | uniq -c | sort -n -r | less
+
+cat $1 | tr [:upper:] [:lower:] | tr -sc 'A-Za-z' '\n' | sort | uniq -c | sort -n -r | less
+
 # use nlp.classifier.Basic
 # training it with each class being an ID (or title) of an article
 # def count_article_words():
@@ -25,4 +48,5 @@ def article_words_matrix(words, article_words):
         if 3 < c < len(article_words) * 0.6:
             interesting_words.append(w)
     return [[(word in aw and aw[word] or 0) for word in interesting_words] for aw in article_words]
+
 
