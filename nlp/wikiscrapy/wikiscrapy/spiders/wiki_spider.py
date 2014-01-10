f3ba2957927ab405b434846cf7d5718409577d38
Hobson Lane <laneh@sharplabs.com>
2014-01-10 12:19:49 -0800

    yield instead of return in parse_response. Also doctext to show how to control it from the bash shell prompt

--- nlp/wikiscrapy/wikiscrapy/spiders/wiki_spider.py
+++ nlp/wikiscrapy/wikiscrapy/spiders/wiki_spider.py
@@ -7,13 +7,20 @@ import dateutil.parser
 from time import sleep
 
 class WikiSpider(CrawlSpider):
+    """Crawls wikipedia starting at the seed page. 
+
+    Rate limited to 1.1 second per article (per wikipedia robots.txt)
+    >>> cd wikiscrapy/wikiscrapy
+    >>> scrapy crawl wiki -o wikipedia_erdos.json -t json
+    """
 
     name = 'wiki'
     allowed_domains = ['en.wikipedia.org', 'en.wiktionary.org']
     start_urls = ['''https://en.wikipedia.org/wiki/Paul_Erd%C5%91s''']
     rules = [
-        Rule(SgmlLinkExtractor(allow=['/wiki/.*']), follow=True, process_links='filter_links'),
-        Rule(SgmlLinkExtractor(allow=['/wiki/.*']), 'parse_response')]
+        Rule(SgmlLinkExtractor(allow=['/wiki/.+']), follow=True, process_links='filter_links', callback='parse_response'),
+        #Rule(SgmlLinkExtractor(allow=['/wiki/.*']), 'parse_response')]
+        ]
 
     def clean_list(self, l):
         ans = ['']
@@ -29,20 +36,36 @@ class WikiSpider(CrawlSpider):
         return ans
 
     def clean_datetime(self, dt):
+        at_i = None
         for i, s in enumerate(dt):
             if s.endswith('at'):
                 dt[i] = dt[i][:-3]
+                at_i = i
+        if at_i is not None and len(dt) > (at_i + 1) and dt[at_i + 1] and not ':' in dt[at_i + 1]:
+            dt = dt[:at_i] + [dt[at_i + 1].strip() + ':' + dt[at_i + 2].strip()]
         ans = ' '.join([s.strip() for s in dt])
-        return dateutil.parser.parse(ans)
+        try:
+            return dateutil.parser.parse(ans)
+        except Exception as e:
+            from traceback import format_exc
+            print format_exc(e) +  '\n^^^ Exception caught ^^^\nWARN: Failed to parse datetime string %r\n      from list of strings %r' % (ans, dt)
+            return ans
 
     def filter_links(self, links):
         print '-'*20 + ' LINKS ' + '-'*20
-        print links
-        print '-'*20 + '-------' + '-'*20
+        print '\n'.join(link.url for link in links)
         sleep(1.1)
+        print '-'*20 + '-------' + '-'*20
         return links
 
     def parse_response(self, response):
+        # TODO: 
+        #   1. check for error pages and slowdown or halt crawling
+        #   2. throttle based on robots.txt
+        #   3. save to database (so that json doesn't have to be loaded manually)
+        #   4. use django Models rather than scrapy.Item model
+        #   5. incorporate into a django app (or make it a django app configurable through a web interface)
+        print '='*20 + ' PARSE ' + '='*20
         sel = Selector(response)
         a = WikiItem()
         a['url'] = response.url
@@ -50,4 +73,6 @@ class WikiSpider(CrawlSpider):
         a['toc'] = ' '.join(self.clean_list(sel.xpath("//div[@id='toc']//ul//text()").extract()))
         a['text'] = ' '.join(sel.xpath('//div[@id="mw-content-text"]//text()').extract())
         a['modified'] = self.clean_datetime(sel.xpath('//li[@id="footer-info-lastmod"]/text()').re(r'([0-9]+\s*\w*)'))
-        return a
+        sleep(1.1)
+        print '='*20 + '=======' + '='*20
+        yield a
