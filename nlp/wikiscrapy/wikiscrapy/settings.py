03c6f2a035f4bea223504b5cd233732c294f2c8f
Hobson Lane <laneh@sharplabs.com>
2014-01-13 16:33:47 -0800

    won't run recursively again

--- nlp/wikiscrapy/wikiscrapy/settings.py
+++ nlp/wikiscrapy/wikiscrapy/settings.py
@@ -11,5 +11,22 @@ BOT_NAME = 'wikiscrapy'
 SPIDER_MODULES = ['wikiscrapy.spiders']
 NEWSPIDER_MODULE = 'wikiscrapy.spiders'
 
+#ROBOTSTXT_OBEY = False
+
+#DEPTH_LIMIT = 0
+#DEPTH_STATS = True
+#DEPTH_PRIORITY = 0
+
+#DUPEFILTER_CLASS = 'scrapy.dupefilter.RFPDupeFilter'
+
+#CONCURRENT_REQUESTS = 8  # 16
+#CONCURRENT_REQUESTS_PER_DOMAIN = 4  # 8
+
+#DOWNLOAD_DELAY = 1.1  # in seconds (requests will be delayed by a minimum of this number of seconds)
+#RANDOMIZE_DOWNLOAD_DELAY = True
+
+#AUTOTHROTTLE_START_DELAY = 2.3  # will also obey the DOWNLOAD_DELAY setting
+#AUTOTHROTTLE_MAX_DELAY = 10.3
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'wikiscrapy (+http://www.yourdomain.com)'
+USER_AGENT = 'SharpLearnerBot/1.0 (+http://www.sharplabs.com)'
--- nlp/wikiscrapy/wikiscrapy/spiders/wiki_spider.py
+++ nlp/wikiscrapy/wikiscrapy/spiders/wiki_spider.py
@@ -4,23 +4,45 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 from wikiscrapy.items import WikiItem
 import dateutil.parser
-from time import sleep
+from datetime import datetime
+from collections import Counter
+
+from nlp.strutil import get_words
+
+#from time import sleep
 
 class WikiSpider(CrawlSpider):
     """Crawls wikipedia starting at the seed page. 
 
-    Rate limited to 1.1 second per article (per wikipedia robots.txt)
-    >>> cd wikiscrapy/wikiscrapy
-    >>> scrapy crawl wiki -o wikipedia_erdos.json -t json
+    Rate limited by obeying robots.txt (see settings.py), autothrottle, and default delay of 1.1
+
+    $ cd nlp/wikiscrapy
+    $ scrapy crawl wiki -o wikipedia_erdos.json -t json
+
+    >>> import subprocess
+    >>> subprocess.check_output('scrapy', 'crawl', 'wiki', stderr=subprocess.STDOUT)
+
     """
 
+    verbosity = 1
     name = 'wiki'
+    download_delay = 1.1
     allowed_domains = ['en.wikipedia.org', 'en.wiktionary.org']
-    start_urls = ['''https://en.wikipedia.org/wiki/Paul_Erd%C5%91s''']
+    
     rules = [
         Rule(SgmlLinkExtractor(allow=['/wiki/.+']), follow=True, process_links='filter_links', callback='parse_response'),
         #Rule(SgmlLinkExtractor(allow=['/wiki/.*']), 'parse_response')]
         ]
+
+    def __init__(self, start_urls=None, *args, **kwargs):
+        self.start_urls = ['''https://en.wikipedia.org/wiki/Paul_Erd%C5%91s''']
+        if start_urls:
+            if isinstance(start_urls, basestring):
+                self.start_urls = [start_urls]
+            else:
+                self.start_urls = list(start_urls)
+        super(WikiSpider, self).__init__(*args, **kwargs)
+
 
     def clean_list(self, l):
         ans = ['']
@@ -52,10 +74,12 @@ class WikiSpider(CrawlSpider):
             return ans
 
     def filter_links(self, links):
-        print '-'*20 + ' LINKS ' + '-'*20
-        print '\n'.join(link.url for link in links)
-        sleep(1.1)
-        print '-'*20 + '-------' + '-'*20
+        if self.verbosity > 1:
+            print '-'*20 + ' LINKS ' + '-'*20
+            print '\n'.join(link.url for link in links)
+        # sleep(1.1)
+        if self.verbosity > 1:
+            print '-'*20 + '-------' + '-'*20
         return links
 
     def parse_response(self, response):
@@ -66,7 +90,8 @@ class WikiSpider(CrawlSpider):
         #   4. use django Models rather than scrapy.Item model
         #   5. incorporate into a django app (or make it a django app configurable through a web interface)
         #   6. incrementally build occurrence matrix rather than saving raw data to django/postgres db
-        print '='*20 + ' PARSE ' + '='*20
+        if self.verbosity > 1:
+            print '='*20 + ' PARSE ' + '='*20
         sel = Selector(response)
         a = WikiItem()
         a['url'] = response.url
@@ -74,6 +99,8 @@ class WikiSpider(CrawlSpider):
         a['toc'] = ' '.join(self.clean_list(sel.xpath("//div[@id='toc']//ul//text()").extract()))
         a['text'] = ' '.join(sel.xpath('//div[@id="mw-content-text"]//text()').extract())
         a['modified'] = self.clean_datetime(sel.xpath('//li[@id="footer-info-lastmod"]/text()').re(r'([0-9]+\s*\w*)'))
-        sleep(1.1)
-        print '='*20 + '=======' + '='*20
+        a['crawled'] = datetime.now()
+        a['count'] = dict(Counter(get_words(a['text'])))
+        if self.verbosity > 1:
+            print '='*20 + '=======' + '='*20
         yield a
