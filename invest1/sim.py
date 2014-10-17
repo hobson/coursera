0e3d8e4daee18b52665353e2182621b74b963bb3
Hobson Lane <hobsonlane@gmail.com>
2014-10-16 23:31:31 -0700

    sim.py works as long as dates and symbols are valid

--- invest1/sim.py
+++ invest1/sim.py
@@ -14,16 +14,62 @@
 
 import argparse
 import sys
+import csv
+import datetime as dt
+
+import QSTK.qstkutil.DataAccess as da
+#t = qstk.dateutil.getNYSEdays(datetime.datetime(2010,1,1), datetime.datetime(2010,2,1), dt.timedelta(hours=16))
+dataobj = da.DataAccess('Yahoo')
+
+
+def get_price(symbol='SPY', date=(2010,1,1), price='close'):
+    if isinstance(date, basestring):
+        date = dt.datetime(*date.split(','))
+    elif isinstance(date, (tuple, list)):
+        date = dt.datetime(*date)
+    symbol = str(symbol).upper().strip()
+    if symbol == '$CASH':
+        return 1.
+    try:
+        return dataobj.get_data([date], [symbol], 'close')[symbol][0]
+    except:
+        return None
+
 
 def main(args):
     print args
+    print vars(args)['funds']
+    print args.funds
+    portfolio = { '$CASH': args.funds }
+    print portfolio
+    csvreader = csv.reader(args.infile, dialect='excel')
+    for row in csvreader:
+        csvwriter = csv.writer(args.outfile, dialect='excel')
+        print ', '.join(row)
+        symbol = row[3]
+        shares = float(row[5])
+        date = dt.datetime(*[int(i) for i in row[:3] + ['16']])
+        sign = 1 - 2 * int(row[4].strip().upper()[0]=='S')
+        # print date, symbol, sign * shares
+        portfolio[symbol] = portfolio.get(symbol, 0) + sign * shares
+        price = float(get_price(symbol=symbol, date=date, price='close'))
+        # print sign * shares * price
+        if -1 <= sign <= 1:
+            portfolio['$CASH'] -= sign * shares * price
+        total = 0.
+        for (k, v) in portfolio.iteritems():
+            get_price(symbol=k, date=date, price='close')
+            # print date, k, price
+            total += v * price 
+        print portfolio
+        csvwriter.writerow([str(date.date()), total])
 
 
 if __name__ == '__main__':
 
     # create the top-level parser for this "sim" module
     parser = argparse.ArgumentParser(prog='sim', description='Simulate trading and predictive analytics algorithms.')
-    parser.add_argument('source',
+    parser.add_argument('--source',
                         default='Yahoo',
                         choices=('Yahoo', 'Google', 'Bloomberg'),
                         help='Name of financial data source to use in da.DataAccess("Name")')
@@ -32,10 +78,11 @@ if __name__ == '__main__':
 
     # create the parser for the "a" command
     parser_trade = subparsers.add_parser('trade', help='Simulate a sequence of trades')
-    parser_trade.add_argument('funds', type=float, 
+    parser_trade.add_argument('funds', type=float,
+                              nargs='?',
                               default=1000000.,
                               help='Initial funds (cash, USD) in portfolio.')
-    parser_trade.add_argument('infile', type=argparse.FileType('r'),
+    parser_trade.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                               help='Path to input CSV file containing a list of trades: y,m,d,sym,BUY/SELL,shares',
                               default=sys.stdin)
     parser_trade.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
@@ -44,3 +91,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     print args
+    main(args)
