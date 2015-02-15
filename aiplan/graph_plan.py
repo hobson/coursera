069aabf3165c2af6d8642d7264543492959131c5
Hobson Lane <hobsonlane@gmail.com>
2015-02-15 14:33:35 -0800

    more random attempts astar and best first return different answer each time

--- aiplan/graph_plan.py
+++ aiplan/graph_plan.py
@@ -17,17 +17,54 @@ from nltk.corpus import wordnet as wn
 infinity = float('inf')
 
 
+"""
+FIXME:
+    - prob(verbosity=0) doesn't take
+
+Examples:
+    >>> node = astar_search(EightPuzzleProblem(initial=[1,6,4,8,7,0,3,2,5], verbosity=0))                      
+    >>> len(node.path())
+    23 or 38
+    >>> node.depth
+    37
+    >>> astar_search(EightPuzzleProblem(initial=[8,1,7,4,5,6,2,0,3], verbosity=0)).depth
+    81
+    >>> # Find the number of unique nodes/states at a given depth (distance) from the goal
+    >>> PDDL parser for planning domains and problems then test code on this problem:
+    (define (problem random-pb1)
+      (:domain random-domain)
+      (:init
+       (S B B) (S C B) (S A C)
+       (R B B) (R C B)
+      )
+     (:goal 
+      (and (S A A))
+     )
+    )
+    >>> # clearly suboptimal, because getting different answer eacy time
+    >>> min(astar_search(EightPuzzleProblem(initial=[1,6,4,8,7,0,3,2,5], verbosity=0)).depth for i in range(100))
+    23
+    >>> min(astar_search(EightPuzzleProblem(initial=[8,1,7,4,5,6,2,0,3], verbosity=0)).depth for i in range(100))
+    31
+    >>> len(nodes_at_depth(EightPuzzleProblem(initial=range(9), verbosity=0), depth=27, verbosity=0))
+    6274
+
+SCRIPS:
+    2 op1 actions are possible
+
+"""
+
 class Problem(object):
     """The abstract class for a graph search problem.
     Should override the methods `.actions()`, and `.result()`
     If result(action) == action then no need to override (this is default `result()`).
     May override `.__init__()`, `.goal_test()`, and `.path_cost()`.
     """
-    def __init__(self, initial, goal=None):  # , graph=nx.Graph()):
+    def __init__(self, initial, goal=None, verbosity=1):  # , graph=nx.Graph()):
         """The constructor specifies the initial state, and possibly a goal
         state, if there is a unique goal.  Your subclass's constructor can add
         other arguments."""
-        self.initial, self.goal = initial, goal
+        self.initial, self.goal, self.verbosity = initial, goal, verbosity
 
     def actions(self, state):
         """Return the actions that can be executed in the given
@@ -46,6 +83,9 @@ class Problem(object):
         """Return True if the state is a goal. The default method compares the
         state to self.goal, as specified in the constructor. Override this
         method if checking against a single self.goal is not enough."""
+        if self.verbosity:
+            print('{0} =? {1}'.format(state, self.goal))
+        state = state.getattr('state', state)
         return force_hashable(state) == force_hashable(self.goal)
 
     def path_cost(self, c, state1, action, state2):
@@ -61,6 +101,8 @@ class Problem(object):
         and related algorithms try to maximize this value."""
         pass
 
+    def pretty_str(self, state):
+        return str(state)
 
 def swap(seq, i0=1, i1=0):
     """Swap elements in a sequence
@@ -129,6 +171,7 @@ class EightPuzzleProblem(Problem):
     N2 = 9
     corners = set((0, N-1, N*(N-1), N2-1))
     verbosity = 1
+    digits = int((N2-1)/10) + 1
 
     def __init__(self, initial=None, goal=tuple(range(N2)), verbosity=None):  # , graph=nx.Graph()):
         """The constructor specifies the initial state, and possibly a goal
@@ -140,16 +183,19 @@ class EightPuzzleProblem(Problem):
             self.N2 = 9
         # FIXME: allow non-square boards
         self.N = int(self.N2 ** .5)
+        self.digits = int((self.N2-1)/10)
         self.initial = range(self.N2)
         self.initial = initial or shuffled(range(self.N2))
         self.goal = goal or list(range(self.N2))
         self.typ = type(goal) if goal else type(initial) if initial else tuple
         if verbosity is not None:
             self.verbosity = int(verbosity)
+        super(EightPuzzleProblem, self).__init__(initial=self.initial, goal=self.goal, verbosity=self.verbosity)
 
-    def actions(self, state):
+
+    def actions(self, state, check_reversible=False):
         if self.verbosity:
-            print('actions({0}). . .'.format(state))
+            print('actions({0}). . .'.format(self.pretty_str(state)))
         i0 = state.index(0)
         # 4 corners
         if i0 == 0:                    # top-left
@@ -160,14 +206,13 @@ class EightPuzzleProblem(Problem):
             possibilities = [swap(state, i0, i0 + 1), swap(state, i0, i0 - self.N)]
         elif i0 == self.N2 - 1:        # bottom-right
             possibilities = [swap(state, i0, i0 - 1), swap(state, i0, i0 - self.N)]
-        # noncorner, top edge  (3 actions)
-        elif i0 < self.N:
+        elif i0 < self.N:              # noncorner, top edge  (3 actions)
             possibilities = [swap(state, i0, i0 - 1), swap(state, i0, i0 + 1), swap(state, i0, i0 + self.N)]
-        elif i0 > self.N2 - self.N:
+        elif i0 > self.N2 - self.N:    # noncorner, bottom edge  (3 actions)
             possibilities = [swap(state, i0, i0 - 1), swap(state, i0, i0 + 1), swap(state, i0, i0 - self.N)]
         elif not i0 % self.N:          # noncorner left edge
             possibilities = [swap(state, i0, i0 - self.N), swap(state, i0, i0 + self.N), swap(state, i0, i0 + 1)]
-        elif not i0 % self.N:          # noncorner right edge
+        elif i0 % self.N == (self.N - 1):          # noncorner right edge
             possibilities = [swap(state, i0, i0 - self.N), swap(state, i0, i0 + self.N), swap(state, i0, i0 - 1)]
         # noncorner, nonedge
         else:
@@ -175,8 +220,21 @@ class EightPuzzleProblem(Problem):
         if self.verbosity:
             print('possibilities = {0}'.format(possibilities))
         # all actions should be reversible
-        assert(all(self.state in self.actions(st) for st in possibilities))
+        if check_reversible:
+            for act in possibilities:
+                if self.verbosity:
+                    print('assert({0} in self.actions({1}) ({2}) )'.format(state, act, self.actions(act, check_reversible=False)))
+                assert(state in self.actions(act, check_reversible=False))
         return possibilities
+
+    def pretty_str(self, state):
+        return str(state)
+        s = ('_' * (self.digits + 1))*self.N + '\n'
+        eol = '\n'
+        for row in range(self.N):
+            s += ' '.join(('{0: '+str(self.digits)+'d}').format(int(state[self.N * row + col])) if row+col else ' '*self.digits for col in range(self.N)) + eol
+        s += ('-' * (self.digits + 1))*self.N + '\n'
+        return s
 
 
 def distance((ax, ay), (bx, by)):
@@ -184,8 +242,37 @@ def distance((ax, ay), (bx, by)):
     return math.hypot((ax - bx), (ay - by))
 
 
-def N_puzzle_heuristic(node):
+def h_npuzzle_simple(node):
     return node.state.index(0)
+
+
+def h_npuzzle_manhattan(node, N2=None, verbosity=None):
+    """Distance of all numbered tiles from their goal position, summed
+
+    Interestingly, this ignores the distance from the goal for the vacant
+    tile (hole or gap).
+
+    References:
+        http://heuristicswiki.wikispaces.com/Manhattan+Distance
+    """
+    N2 = N2 or h_npuzzle_manhattan.N2 or max(node.state) + 1
+    h_npuzzle_manhattan.N2 = N2
+    if verbosity is None:
+        verbosity = h_npuzzle_manhattan.verbosity
+    else:
+        verbosity = verbosity
+        h_npuzzle_manhattan.verbosity = verbosity
+    N = int(N2 ** 0.5)
+    distance = 0 
+    for tile in range(1, N2):
+        pos = node.state.index(tile)
+        distance += abs(pos % N - tile % N) + abs(pos / N - tile / N)
+    if verbosity:
+        print('h_npuzzle_manhattan(node={0}, N2={1}) distance = {2}'.format(node, N2, distance))
+    return distance 
+h_npuzzle_manhattan.N2 = None
+h_npuzzle_manhattan.verbosity = 0
+
 
 # def IterablePriorityQueue(PriorityQueue):
 #     keys = 
@@ -308,7 +395,8 @@ def N_puzzle_heuristic(node):
 #         return hash(self.state)
 
 
-def best_first_graph_search(problem, f):
+# NORVIG's original version of astar_search
+def best_first_graph_search(problem, f=h_npuzzle_manhattan):
     """Search the nodes with the lowest f scores first.
     You specify the function f(node) that you want to minimize; for example,
     if f is a heuristic estimate to the goal, then we have greedy best
@@ -316,7 +404,7 @@ def best_first_graph_search(problem, f):
     There is a subtlety: the line "f = memoize(f, 'f')" means that the f
     values will be cached on the nodes as they are computed. So after doing
     a best first search you can examine the f values of the path returned."""
-    f = memoize(f, 'f')
+    # f = memoize(f, 'f')
     node = Node(problem.initial)
     if problem.goal_test(node.state):
         return node
@@ -338,10 +426,46 @@ def best_first_graph_search(problem, f):
                     del frontier[hashable_incumbent]
                     frontier.append(hashable_child)
     return None
+# END NORVIG AIMA code
 
 
-def astar_search(problem, f=N_puzzle_heuristic):
-    """Norvig's A* graph search algorithm
+# NORVIG's original version of astar_search
+def best_first_tree_search(problem, f=h_npuzzle_manhattan):
+    """Search the nodes with the lowest f scores first.
+    You specify the function f(node) that you want to minimize; for example,
+    if f is a heuristic estimate to the goal, then we have greedy best
+    first search; if f is node.depth then we have breadth-first search.
+    There is a subtlety: the line "f = memoize(f, 'f')" means that the f
+    values will be cached on the nodes as they are computed. So after doing
+    a best first search you can examine the f values of the path returned."""
+    # f = memoize(f, 'f')
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = PriorityQueue(min, f)
+    frontier.append(node)
+    # explored = set()
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node):
+            return node
+        # explored.add(force_hashable(node.state))
+        for child in node.expand(problem):
+            if child not in frontier:
+                frontier.append(child)
+            else:
+                incumbent = frontier[child]
+                if f(child) < f(incumbent):
+                    del frontier[incumbent]
+                    frontier.append(child)
+    return None
+# END NORVIG AIMA code
+
+
+def astar_search(problem, f=h_npuzzle_manhattan):
+    """Modified version of Norvig's A* graph search algorithm
+
+    Allows unhashable states by converting them to hashable types inside Node
 
     Search the nodes with the lowest f scores first.
     You specify the function f(node) that you want to minimize; for example,
@@ -373,57 +497,35 @@ def astar_search(problem, f=N_puzzle_heuristic):
                     frontier.append(hashable_child)
     return None
 
-    f = memoize(f, 'f')
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = PriorityQueue(min, f)
-    frontier.append(node)
-    explored = set()
-    while frontier:
-        node = frontier.pop()
-        if problem.goal_test(node.state):
-            return node
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                frontier.append(child)
-            elif child in frontier:
-                incumbent = frontier[child]
-                if f(child) < f(incumbent):
-                    del frontier[incumbent]
-                    frontier.append(child)
-    return None
 
-# END NORVIG AIMA code
 
-class WordNetProblem(Problem):
-    "The problem of searching a graph from one node to another."
-    def __init__(self, initial='bacon', goal='dancing'):
-        Problem.__init__(self, initial, goal)
-        self.graph = wn
+# class WordNetProblem(Problem):
+#     "The problem of searching a graph from one node to another."
+#     def __init__(self, initial='bacon', goal='dancing'):
+#         Problem.__init__(self, initial, goal)
+#         self.graph = wn
 
-    def actions(self, word):
-        """The actions at a word are just its "neighbors", synonyms, 
-        alternatively the other words in the categories it belongs."""
-        # if isinstance(word, nltk.corpus.reader.wordnet.Synset)
-        return list(ss.split('.')[0] for sss in wn.synsets(word) for ss in sss)
-        # return self.graph.get(A).keys()
+#     def actions(self, word):
+#         """The actions at a word are just its "neighbors", synonyms, 
+#         alternatively the other words in the categories it belongs."""
+#         # if isinstance(word, nltk.corpus.reader.wordnet.Synset)
+#         return list(ss.split('.')[0] for sss in wn.synsets(word) for ss in sss)
+#         # return self.graph.get(A).keys()
 
-    def result(self, state, action):
-        "The result of going to a neighbor is just that neighbor."
-        return action
+#     def result(self, state, action):
+#         "The result of going to a neighbor is just that neighbor."
+#         return action
 
-    def path_cost(self, cost_so_far, A, action, B):
-        return cost_so_far + (self.graph.get(A,B) or infinity)
+#     def path_cost(self, cost_so_far, A, action, B):
+#         return cost_so_far + (self.graph.get(A,B) or infinity)
 
-    def h(self, node):
-        "h function is straight-line distance from a node's state to goal."
-        locs = getattr(self.graph, 'locations', None)
-        if locs:
-            return int(distance(locs[node.state], locs[self.goal]))
-        else:
-            return infinity
+#     def h(self, node):
+#         "h function is straight-line distance from a node's state to goal."
+#         locs = getattr(self.graph, 'locations', None)
+#         if locs:
+#             return int(distance(locs[node.state], locs[self.goal]))
+#         else:
+#             return infinity
 
 
 
@@ -439,50 +541,82 @@ class WordNetProblem(Problem):
 
 
 
-def graph_for_problem(name=None):
-    """Formulate a problem as a networkx Graph with a goal nodes named 'goal' and start nodes named 'start'"""
-    if name in (None, '') or name.lower().strip() in ('', 'null', 'none', 'empty'):
-        name = 'No Problem'.format('N')
-        G = nx.Graph(name=str(name))
-        G.add_node(0, name='start')
-        G.add_node(1, name='goal')
-        G.add_edge(0, 1, action='move')
-        return G
-    name = str(name).lower().strip()
-    if re.match(r'[1-8]{1,2}[-\s]?puz'):
-        N = int(name[0]) + 1
-        M = sqrt(N)
-        assert(M**2 == N)
-        name = '{0} Puzzle'.format(N)
-        initial_positions = np.random.permutation(N)
-        G = nx.Graph(name=str(name))
-        G.add_node(0, name='start')
-        G.add_node(1, name='goal')
-        G.add_edge(0, 1, action='move')
-        return G
-    if 'mission' in name or 'cani' in name:
-        return G 
+# def graph_for_problem(name=None):
+#     """Formulate a problem as a networkx Graph with a goal nodes named 'goal' and start nodes named 'start'"""
+#     if name in (None, '') or name.lower().strip() in ('', 'null', 'none', 'empty'):
+#         name = 'No Problem'.format('N')
+#         G = nx.Graph(name=str(name))
+#         G.add_node(0, name='start')
+#         G.add_node(1, name='goal')
+#         G.add_edge(0, 1, action='move')
+#         return G
+#     name = str(name).lower().strip()
+#     if re.match(r'[1-8]{1,2}[-\s]?puz'):
+#         N = int(name[0]) + 1
+#         M = sqrt(N)
+#         assert(M**2 == N)
+#         name = '{0} Puzzle'.format(N)
+#         initial_positions = np.random.permutation(N)
+#         G = nx.Graph(name=str(name))
+#         G.add_node(0, name='start')
+#         G.add_node(1, name='goal')
+#         G.add_edge(0, 1, action='move')
+#         return G
+#     if 'mission' in name or 'cani' in name:
+#         return G 
 
 
-def search_strategy(frontier):
-    """Return the next node in the frontier to explore"""
-    return frontier[0]
+# def search_strategy(frontier):
+#     """Return the next node in the frontier to explore"""
+#     return frontier[0]
 
 
-def path_to_node(node):
-    pass
+# def path_to_node(node):
+#     pass
 
 
-G = graph_for_problem('null')
+# G = graph_for_problem('null')
 
 
-def plan_path_to_goal(G):
-    """Return a list of edges in graph G that connect the node named 'start' to any node named 'goal'"""
-    frontier = search_nodes(G.nodes(name='start'))
-    node = select_next_node(frontier, strategy)
-    if not frontier:
-        return None
-    if node.name == 'goal':
-        return path_to_node(node)
-    fringe += [node]
+# def plan_path_to_goal(G):
+#     """Return a list of edges in graph G that connect the node named 'start' to any node named 'goal'"""
+#     frontier = search_nodes(G.nodes(name='start'))
+#     node = select_next_node(frontier, strategy)
+#     if not frontier:
+#         return None
+#     if node.name == 'goal':
+#         return path_to_node(node)
+#     fringe += [node]
 
+
+def nodes_at_depth(problem, initial=None, depth=27, verbosity=1):
+    """Breadth-first search to see how many unique states exist at a given distance/depth from `initial`"""
+    if initial is None:
+        initial = problem.goal
+    visited = set()  # set of hashable states already visited
+    frontier = set()   # set of hashable states at fringe being explored now
+    node = Node(problem.initial)
+    frontier.add(node)
+    d = 0
+    while frontier and d < depth:
+        if verbosity:
+            print('='*40 + ' {0:03d}'.format(d) + ' ' + '='*40)
+        d += 1
+        children = set()  # set of nodes to explore next
+        for node in frontier:
+            hashable_state = force_hashable(node.state)
+            visited.add(hashable_state)
+            for child in node.expand(problem):
+                hashable_child = force_hashable(child.state)
+                if hashable_child not in visited:
+                    children.add(child)
+        frontier = set(children)
+        if verbosity > 1:
+            print(','.join(''.join(str(i) for i in node.state) for node in frontier))
+        elif verbosity:
+            print(len(children))
+    return frontier
+
+initial = swap(range(9), 0, 1)
+prob = EightPuzzleProblem(initial=initial, verbosity=0)
+goal_node = astar_search(prob, f=h_npuzzle_manhattan)
